e6f4455d8e2f77d6222e33f662b9514645855864
Moop204 <andrew.nguyen2045@outlook.com>
2019-11-12 20:23:12 +1100

    Added newlines for style and modified instructions

---
+++ frontend/instructions.md
@@ -0,0 +1,7 @@
+# How to Start
+
+Run from frontend folder.
+
+python -m http.server 
+
+If it fails to create a server consider using python3.
--- frontend/js/loginPage.js
+++ frontend/js/loginPage.js
@@ -2,32 +2,41 @@ import { initApp } from "./main.js";
 
 function setupLoginPage() {
     let body_div = document.getElementById("Content");
+
     let login_div = document.createElement("div");
     login_div.id = "LoginForm";
+
     let title = document.createElement("h1");
     title.textContent = "VBMS";
     login_div.appendChild(title);
     body_div.appendChild(login_div);
+
     let loginForm_form = document.createElement("form");
+
     let loginForm_username = document.createElement("input");
     loginForm_username.type = "text";
     loginForm_username.id = "username";
     loginForm_username.required = "true";
     loginForm_username.placeholder = "username";
+
     let username_div = document.createElement("div");
     username_div.appendChild(loginForm_username);
     loginForm_form.appendChild(username_div);
+
     let loginForm_password = document.createElement("input");
     loginForm_password.type = "password";
     loginForm_password.id = "password";
     loginForm_password.required = "true";
     loginForm_password.placeholder = "password";
+
     let password_div = document.createElement("div");
     password_div.appendChild(loginForm_password);
     loginForm_form.appendChild(password_div);
+
     let loginForm_submit = document.createElement("input");
     loginForm_submit.type = "submit";
     loginForm_submit.value = "Login";
+
     let submit_div = document.createElement("div");
     submit_div.appendChild(loginForm_submit);
     loginForm_form.appendChild(submit_div);
