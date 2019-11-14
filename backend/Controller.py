66f1cfc67d22caae632e8e29336556422623f63c
shoshin <shoshinprograms@gmail.com>
2019-11-14 13:00:27 +1100

    implement adding BR in Controller

--- backend/Controller.py
+++ backend/Controller.py
@@ -1,6 +1,7 @@
 from BloodBank import BloodBank
 from Notification import Notification
 from Log import Log
+from BloodRecord import BloodRecord
 
 class Controller():
     def __init__(self):
@@ -9,7 +10,10 @@ class Controller():
         self.bloodBank = BloodBank()
     
     #TODO
-    #def addBR():
+    def addBR(self,bloodType, Location, day1, month1, year1, day2, month2, year2, isOkay):
+        #TODO deal with bloodType enumeration
+        self.bloodBank.Push(bloodType,BloodRecord(bloodType,Location,day1,month1,year1,day2,month2,year2,isOkay))
+        #TODO log and Notif
     #def ViewNotif():
     #def ViewLog():
     #def ViewBR():
