7b7498e3708c226f7dad404cb80daf2c4166a2a0
shoshin <shoshinprograms@gmail.com>
2019-11-20 00:26:32 +1100

    Implement sign out done

--- frontend/index.js
+++ frontend/index.js
@@ -1,3 +1,8 @@
 import { initApp } from "./js/main.js";
 
+let signOut_li = document.getElementById("signOut");
+signOut_li.addEventListener("click", e => {
+    let userType = localStorage.removeItem("userType");
+    initApp();
+})
 initApp();
